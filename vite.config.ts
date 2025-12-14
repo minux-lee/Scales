ee91d8169755f79eb06cd247769cbe74d6f028a7
Ryan Lee <minwook.lee@pm.me>
2025-12-14 11:34:01 +0900

    fix: github page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/ChecK/",
   server: {
     headers: {
       // SharedArrayBuffer 사용을 위한 보안 헤더 (WebChucK 필수)
